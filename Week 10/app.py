af6a842a6dd986d73e8af2f4a3b4e4bf2d22a876
Ammaz <withammazch@gmail.com>
2026-05-16 17:19:25 +0500

    main update

--- Week 10/app.py
+++ Week 10/app.py
@@ -17,19 +17,50 @@ def write_file(username, password):
     file.write(username + "," + password + "\n")
     file.close()
 
-
 def login():
-    pass
+    users = read_file()
+    user = entry_user.get()
+    pwd = entry_pass.get()
 
+    if user in users and users[user] == pwd:
+        result.config(text="Login Successful")
+    else:
+        result.config(text="Wrong Username or Password")
+
+
+# Signup function
 def signup():
-    pass    
+    user = entry_user.get()
+    pwd = entry_pass.get()
+    users = read_file()
+
+    if user in users:
+        result.config(text="User Already Exists")
+    else:
+        write_file(user, pwd)
+        result.config(text="Signup Successful")
+ 
 
 def main():
-    pass
+    global entry_user, entry_pass, result
 
+    tk.Label(root, text="Username").pack()
+    entry_user = tk.Entry(root)
+    entry_user.pack()
+
+    tk.Label(root, text="Password").pack()
+    entry_pass = tk.Entry(root, show="*")
+    entry_pass.pack()
+
+    tk.Button(root, text="Login", command=login).pack()
+    tk.Button(root, text="Signup", command=signup).pack()
+
+    result = tk.Label(root, text="")
+    result.pack()
 root = tk.Tk()
 root.title("Login System")    
+root.geometry("300x200")
 
 
-
+main()
 root.mainloop()
