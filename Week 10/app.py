338fd1cfa9b2408919c7245153f620f037c6c72d
Ammaz <withammazch@gmail.com>
2026-05-16 17:17:09 +0500

    read function added

--- Week 10/app.py
+++ Week 10/app.py
@@ -1,8 +1,16 @@
 import tkinter as tk
 # login reading from file
 def read_file():
-    
-    pass
+    users = {}
+    try:
+        file = open("users.txt", "r")
+        for line in file:
+            username, password = line.strip().split(",")
+            users[username] = password
+        file.close()
+    except:
+        pass
+    return users
 
 def write_file():
     pass
